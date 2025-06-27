0b8e5d2b940d8425c6fa43de8f56210c0e5117e1
sreedevk <sreedev@icloud.com>
2025-06-27 15:01:04 +0000

    index out of bounds bug fixed

--- src/app.nim
+++ src/app.nim
@@ -45,40 +45,13 @@ proc setupDisplayBuffer*(e: var App) =
   e.buffer.clear()
   e.buffer.setForegroundColor(fgBlack, true)
   e.buffer.drawRect(0, 0, e.tw - 1, e.th - 1)
-
   e.buffer.setForegroundColor(fgMagenta)
   e.buffer.drawHorizLine(2, e.tw - 3, yoffset + 1, doubleStyle = true)
   e.buffer.write(2, yoffset + 2, "OPDS TUI")
   e.buffer.drawHorizLine(2, e.tw - 3, yoffset + 3, doubleStyle = true)
-  e.buffer.write(
-    2, yoffset + 4, fgWhite, "Press ", fgYellow, "Enter", fgWhite, " to select an item."
-  )
-  e.buffer.write(
-    2,
-    yoffset + 5,
-    "Press ",
-    fgYellow,
-    "ESC",
-    fgWhite,
-    " or ",
-    fgYellow,
-    "Q",
-    fgWhite,
-    " to quit or navigate back to the previous page",
-  )
-  e.buffer.write(
-    2,
-    yoffset + 6,
-    "Press ",
-    fgYellow,
-    "N",
-    fgWhite,
-    " or ",
-    fgYellow,
-    "P",
-    fgWhite,
-    " to paginate to the next or previous page.",
-  )
+  e.buffer.write(2, yoffset + 4, fgWhite, "Press ", fgYellow, "Enter", fgWhite, " to select an item.")
+  e.buffer.write(2, yoffset + 5, "Press ", fgYellow, "ESC", fgWhite, " or ", fgYellow, "Q", fgWhite, " to quit or navigate back to the previous page")
+  e.buffer.write(2, yoffset + 6, "Press ", fgYellow, "N", fgWhite, " or ", fgYellow, "P", fgWhite, " to paginate to the next or previous page.")
   if e.session.pages[^1].canSearch:
     e.buffer.write(2, yoffset + 7, "Press ", fgYellow, "/", fgWhite, " to search")
 
@@ -120,15 +93,18 @@ proc handleUserInputBrowseMode(e: var App) =
     else:
       cpage.entryPtr = len(cpage.entries) - 1
   of Key.Enter:
-    let centry = cpage.entries[cpage.entryPtr]
-    case centry.linkType
-    of LinkType.Navigation:
-      e.session.navigateTo(centry.url)
-    of LinkType.Media:
-      let content = e.session.loadFile(centry.url)
-      let zathproc = "zathura --fork - --"
-      discard execCmdEx(zathproc, input = content)
-      exitProc()
+    if len(cpage.entries) > 0:
+      let centry = cpage.entries[cpage.entryPtr]
+      case centry.linkType
+      of LinkType.Navigation:
+        e.session.navigateTo(centry.url)
+      of LinkType.Media:
+        let content = e.session.loadFile(centry.url)
+        let zathproc = "zathura --fork - --"
+        discard execCmdEx(zathproc, input = content)
+        exitProc()
+    else:
+      discard
   else:
     discard
 
